0b8531a6538f3f276760a0cb4170f0c1f5082024
rawat208 <rawatdeepak01239@gmail.com>
2026-04-06 13:04:41 +0530

    ffds

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const routes = require("./routes/userroutes")    
+const dotenv = require("dotenv")
+dotenv.config()
 
 const app = express()
 app.use(express.json())
+const port = process.env.port || 8000   
 
-const port = 5000;
 
-const mogourl =  "mongodb+srv://sanjay:" 
-
-mongoose.connect(mogourl)
+mongoose.connect(process.env.mogourl)
 
 .then(() => {console.log(`mongo db connect `)})
 .catch((e) =>{console.log(`mongo db error${e}`)})
 
 app.use("/", routes)
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`)})
+app.listen(process.env.port, () => {console.log(`Server is running on port ${process.env.port}`)})
 
+
+ 
